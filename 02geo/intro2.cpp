449434f61eaf46bb6a450544392dcbac2e3b8251
itgimpi <brejkersi@gmail.com>
2023-11-16 12:27:58 +0100

    strukture

--- 02geo/intro2.cpp
+++ 02geo/intro2.cpp
@@ -25,9 +25,9 @@ typedef struct {
 typedef struct {
     point a, b, c; }  triangle;
 
-typedef struct { // mnougao, temena su numerisana redom!!!
-    int n; // broj Temena
-    point pol[100]; } polygone; // najvise 100-ugao
+typedef struct {
+    int n;
+    point pol[100]; } polygone;
 
 double dist0( point a ) { // rastojanje od koord. pocetka
     return sqrt ( a.x * a.x + a.y * a.y ); }
@@ -38,25 +38,19 @@ double dist03d( point3d a ) { // rastojanje od koord. pocetka u 3d
 double dist( point a, point b ) { // rast. izmedju 2 tacke
     return sqrt ( (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) ); }
 
-double triangle_per( triangle t ) { // obim trougla
+double triangle_per( triangle t ) {
     return dist( t.a, t.b ) + dist( t.b, t.c ) + dist( t.c, t.a ); }
 
-double triangle_area( triangle t ) { // povrsina trougla, Heron
-    double s = triangle_per(t) / 2.0;
+double triangle_area( triangle t ) {
+    double s = triangle_per( t ) / 2.0;
     return sqrt( s * ( s - dist( t.a, t.b ) ) * ( s - dist( t.b, t.c ) ) * ( s - dist( t.c, t.a ) ) ); }
 
-double polygone_per ( polygone p ) {
+double polygone_per( polygone p ) {
     double per = 0.0;
-    for ( int i = 0; i < p.n - 1; i++) // neka je n = 6
-        per += dist( p.pol[i], p.pol[i+1] ); // 01, 12, 23, 34, 45 
-    per += dist( p.pol[p.n-1], p.pol[0] );   // 50
+    for ( int i = 0; i < p.n-1; i++ ) // n = 6, n - 2 = 4
+        per += dist(p.pol[i], p.pol[i+1]); // d01 + d12 + d23 + d34 + d45
+    per += dist(p.pol[p.n-1], p.pol[0]);   
     return per; }
-
-double polygone_area ( polygone p ) {
-
-
-
-     }
 
 int main() {
     point a, b, c;
@@ -72,6 +66,13 @@ int main() {
     cout << dist( a, b ) << endl;
     cout << triangle_per( t ) << endl;
     cout << triangle_area( t ) << endl;
+
+    polygone p; p.n = 3;
+    p.pol[0].x = 1.0; p.pol[0].y = 1.0;
+    p.pol[1].x = 2.0; p.pol[1].y = 1.0;
+    p.pol[2].x = 2.0; p.pol[2].y = 2.0;
+    cout << polygone_per( p ) << endl;
+
     
     
 
